correct computation of c1 after prob of error introduced. plotters now handle prob of error too.

src/figures_res_synth.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from helpers import read_results_synth
 
 
-def plot_SNbm(path, eps1, eps2):
+def plot_SmbN(path, eps1, eps2):
     """
     Plot synthetic metrics as a function of N.
 
@@ -19,7 +19,7 @@ def plot_SNbm(path, eps1, eps2):
     Plots synthetic results as
     N by metrics(d) for each ld, k, method, metric
     """
-    seeds, lds, ds, Ns, _, ks, methods, metrics, results = \
+    seeds, lds, pes, ds, Ns, _, ks, methods, metrics, results = \
         read_results_synth(path)
     ds.sort(key=float)
 
@@ -30,108 +30,103 @@ def plot_SNbm(path, eps1, eps2):
         for method in methods:
             for k in ks:
                 for ld in lds:
-                    # Now in the same figure
-                    markers = cycle(markers_list)
-                    _, ax = plt.subplots()
-                    for d in ds:
-                        # Remove some d here
-                        # So that the plot is not dense
-                        if (int(d)-10) % 80 != 0:
-                            continue
-                        if ld == '1.00':
-                            line = eps1*np.ones(Ns[d].size)
-                        else:
-                            line = eps2*np.ones(Ns[d].size)
-                        x = Ns[d]
-                        y = np.zeros((len(seeds), x.size))
-                        for i, seed in enumerate(seeds):
-                            y[i] = results[seed][ld][d][k][method][metric]
-                        # Mean and std of metrics
-                        my = y.mean(axis=0)
-                        sdy = y.std(axis=0)
+                    for pe in pes:
+                        # Now in the same figure
+                        markers = cycle(markers_list)
+                        _, ax = plt.subplots()
+                        for d in ds:
+                            # Remove some d here
+                            # So that the plot is not dense
+                            if (int(d)-10) % 80 != 0:
+                                continue
+                            if ld == '1.00':
+                                line = eps1*np.ones(Ns[d].size)
+                            else:
+                                line = eps2*np.ones(Ns[d].size)
+                            x = Ns[d]
+                            y = np.zeros((len(seeds), x.size))
+                            for i, seed in enumerate(seeds):
+                                y[i] = results[seed][ld][pe][d][k][method][metric]
+                            # Mean and std of metrics
+                            my = y.mean(axis=0)
+                            sdy = y.std(axis=0)
 
-                        plt.plot(x, my, next(markers), label=r'$d = %s$' % d,
-                                 markersize=3)
-                        plt.fill_between(x, my - sdy, my + sdy, alpha=0.2)
-                    if metric == 'err_norm':
-                        plt.plot(x, line, 'k-.')
-                    if metric == 'err_angle':
-                        label = r'$\angle(\hat\beta, \beta)$'
-                        lim = 1.5
-                    elif metric == 'err_norm':
-                        if method == '1':
-                            label = r'$||\hat\beta - c_1\beta||$'
-                        elif method == '2':
-                            label = r'$||\hat\beta - \beta||$'
-                        lim = 4
-                    else:
-                        label = r'$\tau(\hat\beta, \beta)$'
-                        lim = 0.4
-                    ax.annotate(r'$N$', xy=(.95, 0), xytext=(18, -5),
-                                ha='left', va='top', xycoords='axes fraction',
-                                textcoords='offset points', fontsize=16)
-                    plt.ylabel(label, fontsize=16)
-                    # plt.xscale('log')
-                    plt.ylim(0, lim)
-                    plt.grid()
-                    plt.legend(loc='upper right', fontsize=10)
-                    plt.tight_layout()
-                    plt.savefig(path+'../Syn-Nbm-%s-%s-%s-%s.pdf'
-                                % (metric, ld, k, method),
-                                format='pdf', transparent=True)
-                    plt.close()
+                            plt.plot(x, my, next(markers), label=r'$d = %s$' % d,
+                                     markersize=3)
+                            plt.fill_between(x, my - sdy, my + sdy, alpha=0.2)
+                        if metric == 'err_angle':
+                            label = r'$\angle(\hat\beta, \beta)$'
+                            lim = 1.5
+                        elif metric == 'err_norm':
+                            plt.plot(x, line, 'k-.')
+                            if method == '1':
+                                label = r'$||\hat\beta - c_1\beta||$'
+                            elif method == '2':
+                                label = r'$||\hat\beta - \beta||$'
+                            lim = 4
+                        else:
+                            label = r'$\tau(\hat\beta, \beta)$'
+                            lim = 0.4
+                        ax.annotate(r'$N$', xy=(.95, 0), xytext=(18, -5),
+                                    ha='left', va='top', xycoords='axes fraction',
+                                    textcoords='offset points', fontsize=16)
+                        plt.ylabel(label, fontsize=16)
+                        # plt.xscale('log')
+                        plt.ylim(0, lim)
+                        plt.grid()
+                        plt.legend(loc='upper right', fontsize=10)
+                        plt.tight_layout()
+                        plt.savefig(path+'../Syn-Nbm-%s-%s-%s-%s-%s.pdf'
+                                    % (metric, ld, pe, k, method),
+                                    format='pdf', transparent=True)
+                        plt.close()
 
 
 def plot_SNbd(path, eps1, eps2):
     """
     Plot minimum N that achieves epsilon by d
     """
-    seeds, lds, ds, Ns, _, ks, _, _, results = read_results_synth(path)
+    seeds, lds, pes, ds, Ns, _, ks, _, _, results = read_results_synth(path)
     size = len(Ns[ds[0]])
     ds.sort(key=float)
-    ks.sort(key=float)
+    pes.sort(key=float)
     markers_list = ['x-', 's-', '^-', 'o-', '>-']
 
     x = [int(d) for d in ds]
 
     plt.rc('font', family='serif')
     for k in ks:
-        # Now in the same figure
-        _, ax = plt.subplots()
-        markers = cycle(markers_list)
         for ld in lds:
-            if float(ld) == 1:
-                epsilon = eps1
-            else:
-                epsilon = eps2
-            min_N = np.zeros(len(ds))
-            for j, d in enumerate(ds):
-                y = np.zeros((len(seeds), size))
-                for i, seed in enumerate(seeds):
-                    value = results[seed][ld][d][k]['1']['err_norm']
-                    try:
-                        if (value > 0).any():
-                            y[i] = value
-                    except:
-                        print('Missing file.')
-                        print('%s-%s-%s-%s' % (seed, ld, d, k))
-
-                # Mean and std of metrics
-                my = y.mean(axis=0)
-                loc = np.where(epsilon > my)[0][0]
-                min_N[j] = Ns[ds[0]][loc]
-            label = r'$\lambda_d=%s$' % ld
-            plt.plot(x, min_N, next(markers), label=label, markersize=3)
-        plt.legend(loc='upper left', fontsize=10)
-        plt.grid()
-        plt.ylabel(r'$N$', fontsize=16)
-        ax.annotate(r'$d$', xy=(.95, 0), xytext=(18, -5),
-                    ha='left', va='top', xycoords='axes fraction',
-                    textcoords='offset points', fontsize=16)
-        plt.tight_layout()
-        plt.savefig(path+'../Syn-dbN-%.1f-%s.pdf' % (epsilon, k),
-                    format='pdf', transparent=True)
-        plt.close()
+            # Now in the same figure
+            _, ax = plt.subplots()
+            markers = cycle(markers_list)
+            for pe in pes:
+                if float(ld) == 1:
+                    epsilon = eps1
+                else:
+                    epsilon = eps2
+                min_N = np.zeros(len(ds))
+                for j, d in enumerate(ds):
+                    y = np.zeros((len(seeds), size))
+                    for i, seed in enumerate(seeds):
+                        value = results[seed][ld][pe][d][k]['1']['err_norm']
+                        y[i] = value
+                    # Mean and std of metric
+                    my = y.mean(axis=0)
+                    loc = np.where(epsilon > my)[0][0]
+                    min_N[j] = Ns[ds[0]][loc]
+                label = r'$\p_e=%s$' % pe
+                plt.plot(x, min_N, next(markers), label=label, markersize=3)
+            plt.legend(loc='upper left', fontsize=10)
+            plt.grid()
+            plt.ylabel(r'$N$', fontsize=16)
+            ax.annotate(r'$d$', xy=(.95, 0), xytext=(18, -5),
+                        ha='left', va='top', xycoords='axes fraction',
+                        textcoords='offset points', fontsize=16)
+            plt.tight_layout()
+            plt.savefig(path+'../Syn-dbN-%.1f-%s-%s.pdf' % (epsilon, ld, k),
+                        format='pdf', transparent=True)
+            plt.close()
 
 
 if __name__ == '__main__':
@@ -143,5 +138,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     home_path = str(Path.home())
-    #plot_SNbm(home_path + '/Res-Synth/', args.eps1, args.eps2)
+    plot_SmbN(home_path + '/Res-Synth/', args.eps1, args.eps2)
     plot_SNbd(home_path + '/Res-Synth/', args.eps1, args.eps2)

src/figures_res_synth_by_M.py
@@ -15,7 +15,7 @@ def plot_SmbM(path):
     Assumes all experiments are finished
     and all results are saved.
     """
-    seeds, lds, ds, Ns, Ms, methods, _, results = \
+    seeds, lds, pes, ds, Ns, Ms, methods, _, results = \
         read_results_synth_by_M(path)
     ds.sort(key=float)
 
@@ -23,41 +23,42 @@ def plot_SmbM(path):
     markers_list = ['x-', 's-', '^-', 'o-', '>-']
     for method in methods:
         for ld in lds:
-            # Now in the same figure
-            markers = cycle(markers_list)
-            _, ax = plt.subplots()
-            for d in ds:
-                N = Ns[d]
-                M = Ms[d]
-                x = M
-                y = np.zeros((len(seeds), x.size))
-                for i, seed in enumerate(seeds):
-                    y[i] = results[seed][ld][d][method]['err_norm']
-                # Mean and std of metrics
-                my = y.mean(axis=0)
-                sdy = y.std(axis=0)
+            for pe in pes:
+                # Now in the same figure
+                markers = cycle(markers_list)
+                _, ax = plt.subplots()
+                for d in ds:
+                    N = Ns[d]
+                    M = Ms[d]
+                    x = M
+                    y = np.zeros((len(seeds), x.size))
+                    for i, seed in enumerate(seeds):
+                        y[i] = results[seed][ld][pe][d][method]['err_norm']
+                    # Mean and std of metrics
+                    my = y.mean(axis=0)
+                    sdy = y.std(axis=0)
 
-                plt.plot(x, my, next(markers), label=r'$d = %s$' % d,
-                         markersize=3)
-                plt.fill_between(x, my - sdy, my + sdy, alpha=0.2)
-            # plt.axvline(N)
-            plt.axvline(N*np.log(N), ls='--', color='k', label=r'$M=N\log N$')
-            if method == '1':
-                label = r'$||\hat\beta - c_1\beta||$'
-            elif method == '2':
-                label = r'$||\hat\beta - \beta||$'
-            ax.annotate(r'$M$', xy=(.95, 0), xytext=(15, -5),
-                        ha='left', va='top', xycoords='axes fraction',
-                        textcoords='offset points', fontsize=16)
-            plt.ylabel(label, fontsize=16)
-            plt.xscale('log')
-            plt.ylim(0, 3)
-            plt.grid()
-            plt.legend(loc='upper right', fontsize=10)
-            plt.tight_layout()
-            plt.savefig(path+'../Syn-mbM-%s-%s.pdf' % (ld, method),
-                        format='pdf', transparent=True)
-            plt.close()
+                    plt.plot(x, my, next(markers), label=r'$d = %s$' % d,
+                             markersize=3)
+                    plt.fill_between(x, my - sdy, my + sdy, alpha=0.2)
+                # plt.axvline(N)
+                plt.axvline(N*np.log(N), ls='--', color='k', label=r'$M=N\log N$')
+                if method == '1':
+                    label = r'$||\hat\beta - c_1\beta||$'
+                elif method == '2':
+                    label = r'$||\hat\beta - \beta||$'
+                ax.annotate(r'$M$', xy=(.95, 0), xytext=(15, -5),
+                            ha='left', va='top', xycoords='axes fraction',
+                            textcoords='offset points', fontsize=16)
+                plt.ylabel(label, fontsize=16)
+                plt.xscale('log')
+                plt.ylim(0, 3)
+                plt.grid()
+                plt.legend(loc='upper right', fontsize=10)
+                plt.tight_layout()
+                plt.savefig(path+'../Syn-mbM-%s-%s-%s.pdf' % (ld, pe, method),
+                            format='pdf', transparent=True)
+                plt.close()
 
 
 if __name__ == '__main__':

src/helpers.py
@@ -53,8 +53,10 @@ def read_results_synth(path):
     seeds = list({f[0] for f in sfiles})
     # Different ld values
     lds = list({f[1] for f in sfiles})
+    # Different pe values
+    pes = list({f[2] for f in sfiles})
     # Different d values
-    ds = list({f[2] for f in sfiles})
+    ds = list({f[3] for f in sfiles})
     # Different k values
     ks = list({f[-2] for f in sfiles})
     # Different method values
@@ -69,17 +71,19 @@ def read_results_synth(path):
         results[seed] = {}
         for ld in lds:
             results[seed][ld] = {}
-            for d in ds:
-                results[seed][ld][d] = {}
-                for k in ks:
-                    results[seed][ld][d][k] = {}
-                    for method in methods:
-                        results[seed][ld][d][k][method] = {}
-                        for metric in metrics:
-                            results[seed][ld][d][k][method][metric] = None
+            for pe in pes:
+                results[seed][ld][pe] = {}
+                for d in ds:
+                    results[seed][ld][pe][d] = {}
+                    for k in ks:
+                        results[seed][ld][pe][d][k] = {}
+                        for method in methods:
+                            results[seed][ld][pe][d][k][method] = {}
+                            for metric in metrics:
+                                results[seed][ld][pe][d][k][method][metric] = None
 
     for f in files:
-        seed, ld, d, _, _, k, method = f.split('-')
+        seed, ld, pe, d, _, _, k, method = f.split('-')
 
         with open(path+f, 'rb') as fb:
             resd = pickle.load(fb)  # Results dictionary
@@ -91,9 +95,9 @@ def read_results_synth(path):
             metric_values = np.zeros(Ns[d].size)
             for i, N in enumerate(resd['Ns']):
                 metric_values[i] = resd[N][metric]
-            results[seed][ld][d][k][method][metric] = metric_values
+            results[seed][ld][pe][d][k][method][metric] = metric_values
 
-    return seeds, lds, ds, Ns, Ms, ks, methods, metrics, results
+    return seeds, lds, pes, ds, Ns, Ms, ks, methods, metrics, results
 
 
 def read_results_synth_by_M(path):
@@ -112,8 +116,10 @@ def read_results_synth_by_M(path):
     seeds = list({f[0] for f in sfiles})
     # Different ld values
     lds = list({f[1] for f in sfiles})
+    # Different pe values
+    pes = list({f[2] for f in sfiles})
     # Different d values
-    ds = list({f[2] for f in sfiles})
+    ds = list({f[3] for f in sfiles})
     # Different method values
     methods = list({f[-1] for f in sfiles})
     # Different N and M values for each d
@@ -126,15 +132,17 @@ def read_results_synth_by_M(path):
         results[seed] = {}
         for ld in lds:
             results[seed][ld] = {}
-            for d in ds:
-                results[seed][ld][d] = {}
-                for method in methods:
-                    results[seed][ld][d][method] = {}
-                    for metric in metrics:
-                        results[seed][ld][d][method][metric] = None
+            for pe in pes:
+                results[seed][ld][pe] = {}
+                for d in ds:
+                    results[seed][ld][pe][d] = {}
+                    for method in methods:
+                        results[seed][ld][pe][d][method] = {}
+                        for metric in metrics:
+                            results[seed][ld][pe][d][method][metric] = None
 
     for f in files:
-        seed, ld, d, _, M, method = f.split('-')
+        seed, ld, pe, d, _, M, method = f.split('-')
 
         with open(path+f, 'rb') as fb:
             resd = pickle.load(fb)  # Results dictionary
@@ -146,9 +154,9 @@ def read_results_synth_by_M(path):
             metric_values = np.zeros(Ms[d].size)
             for i, M in enumerate(resd['Ms']):
                 metric_values[i] = resd[M][metric]
-            results[seed][ld][d][method][metric] = metric_values
+            results[seed][ld][pe][d][method][metric] = metric_values
 
-    return seeds, lds, ds, Ns, Ms, methods, metrics, results
+    return seeds, lds, pes, ds, Ns, Ms, methods, metrics, results
 
 
 def check_exp(args, name):
@@ -193,7 +201,7 @@ def get_exp_path(args, name):
     return file_path
 
 
-def get_c1(beta, f_cov):
+def get_c1(alpha, beta, f_cov):
     """
     Estimate c1 = 4E[sigmoid'(beta^T(X-Y))]
     """
@@ -207,9 +215,9 @@ def get_c1(beta, f_cov):
     if np.trapz(pdf, x) < 0.999:
         print('c1 accuracy may be low.')
     # sigmoid(x)
-    sig_x = (1 + np.exp(-x))**-1
+    sig_x = (1+np.exp(-alpha*x))**-1
     # sigmoid'(x)pdf(x)
-    y = sig_x*(1-sig_x)*pdf
+    y = alpha*sig_x*(1-sig_x)*pdf
     e_c1 = 4*np.trapz(y, x)
 
     return e_c1

src/loss.py
@@ -5,7 +5,7 @@ import numpy as np
 from helpers import get_c1
 
 
-def beta_error(e_beta, beta, f_cov, method):
+def beta_error(e_beta, beta, method, e_c1):
     """
     Find the cosine of angle between e_beta and beta
     Estimate c1 by the approximation of sigmoid integral
@@ -19,7 +19,6 @@ def beta_error(e_beta, beta, f_cov, method):
     # If averaging
     if method == 1:
         # Get estimated c1
-        e_c1 = get_c1(beta, f_cov)
         err_beta = np.linalg.norm(e_beta - e_c1*beta)
     # else logistic
     else:

synthetic_by_M.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from time import time
 import numpy as np
-from src.helpers import get_f_stats, save_results, check_exp
+from src.helpers import get_f_stats, save_results, check_exp, get_alpha, get_c1
 from src.data import get_data
 from src.estimators import estimate_beta
 from src.loss import beta_error, kt_distance
@@ -51,14 +51,16 @@ if __name__ == "__main__":
 
     # Start Experiment if not already finished
     check_exp(args, 'synth_by_M')
+
     # Get M values to run for
     Ms = np.logspace(np.log10(300), np.log10(M)).astype(np.int32)
-
     # Beta and feature stats change with seed
     beta = np.random.multivariate_normal(np.zeros(d), np.eye(d)*10)
     f_mean, f_cov = get_f_stats(d, ld)
     # Get alpha that results in prob of error
     alpha = get_alpha(pe, beta, f_cov)
+    e_c1 = get_c1(alpha, beta, f_cov)
+    print('pe: %.1f | alpha: %.3f | c1: %.3f' % (pe, alpha, e_c1))
 
     # This for can be run embarrasingly parallel.
     # But I'm embarrassingly lazy to do that.
@@ -69,7 +71,7 @@ if __name__ == "__main__":
         # Estimate beta
         e_beta = estimate_beta(X, XC, yn, method)
         # Calculate error of beta
-        err_angle, err_norm = beta_error(e_beta, beta, f_cov, method)
+        err_angle, err_norm = beta_error(e_beta, beta, method, e_c1)
         # Test e_beta on new data for kendall tau
         test_X, _, _, _ = get_data(500, 1, beta, f_mean, f_cov, alpha)
         scores = test_X @ beta