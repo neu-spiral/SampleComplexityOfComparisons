15cf34ac7eb46bf299c370b1c6e6080f04ab035c
BerkanKadioglu <kadioglub@ece.neu.edu>
2021-10-29 22:44:11 +0300

    by d and by lambda bash files updated

--- main_by_d.bash
+++ main_by_d.bash
@@ -2,17 +2,17 @@
 
 for seed in {0..9..1}
 do
-    for ld in 0.005 0.1 1
+    for ld in 0.005 1
     do
-        for pe in 0 0.2 0.4
+        for pe in 0 0.4
         do
-            for d in {10..250..20}
+            for d in {10..100..10}
             do
                 for k in 3
                 do
                     for method in 1
                     do
-                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=z.out --error=z.err single.bash $seed $ld $pe $d 300 30000 $k $method
+                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=z.out --error=z.err single.bash $seed $ld $pe $d 100 1000 $k $method
                     done
                 done
             done
