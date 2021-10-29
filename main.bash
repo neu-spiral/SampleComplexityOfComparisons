9d6ebd7a4b934cc5a5e417bd6f7fb409fd151ce3
BerkanKadioglu <kadioglub@ece.neu.edu>
2021-10-29 21:44:48 +0300

    main bash updated

--- main.bash
+++ main.bash
@@ -6,14 +6,14 @@ do
     do
         for pe in 0 0.4
         do
-            for d in 10 250
+            for d in 10 100
             do
                 for k in 3
                 do
                     for method in 1 3
                     do
-                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=z.out --error=z.err single.bash $seed $ld $pe $d 300 30000 $k $method
-                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=zM.out --error=zM.err single_by_M.bash $seed $ld $pe $d 1000 30000 $method
+                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=z.out --error=z.err single.bash $seed $ld $pe $d 100 1000 $k $method
+                        sbatch --job-name=$seed${ld:(-2)}$d$k$method --output=zM.out --error=zM.err single_by_M.bash $seed $ld $pe $d 1000 15000 $method
                     done
                 done
             done
